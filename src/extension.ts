bb84bce0edbd11ceab894b007f50a02b523bec89
Oliver Sun <oliver@sunhx.cn>
2024-03-14 09:35:44 +0800

    add react webview project

--- src/extension.ts
+++ src/extension.ts
@@ -1,6 +1,8 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
+import { getUri } from './utils/getUri';
+import { getNonce } from './utils/getNonce';
 
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
@@ -14,9 +16,64 @@ export function activate(context: vscode.ExtensionContext) {
 	// Now provide the implementation of the command with registerCommand
 	// The commandId parameter must match the command field in package.json
 	let disposable = vscode.commands.registerCommand('compressed-file-editor.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from Compressed File Editor!');
+		// Create HTML
+        const panel = vscode.window.createWebviewPanel(
+				'compressedFileEditor',
+				'Compressed File Editor',
+				vscode.ViewColumn.One,
+				{
+					enableFindWidget: true,
+					enableScripts: true,
+					localResourceRoots: [
+						vscode.Uri.joinPath(context.extensionUri, "out"),
+						vscode.Uri.joinPath(context.extensionUri, "webview/build")
+					],
+					retainContextWhenHidden: true
+				}
+			);
+			
+			const iconPath = getUri(panel.webview, context.extensionUri, ["webview", "build", "logo192.png"]);
+			
+			panel.iconPath = iconPath;
+
+			const stylesUri = getUri(panel.webview, context.extensionUri, [
+				"webview",
+				"build",
+				"static",
+				"css",
+				"main.css",
+			]);
+
+			const scriptUri = getUri(panel.webview, context.extensionUri, [
+				"webview",
+				"build",
+				"static",
+				"js",
+				"main.js",
+			]);
+
+			const nonce = getNonce();
+			
+			panel.webview.html=/*html*/ `
+				<!DOCTYPE html>
+				<html lang="en">
+					<head>
+						<meta charset="utf-8">
+						<meta name="viewport" content="width=device-width,initial-scale=1,shrink-to-fit=no">
+						<meta name="theme-color" content="#000000">
+						<meta http-equiv="Content-Security-Policy" content="default-src 'self' ; style-src ${panel.webview.cspSource}; script-src 'nonce-${nonce}';">
+		
+						<link rel="stylesheet" type="text/css" href="${stylesUri}">
+						<title>Hello World</title>
+					</head>
+					<body>
+						<noscript>You need to enable JavaScript to run this app.</noscript>
+						<div id="root"></div>
+						<script nonce="${nonce}" src="${scriptUri}"></script>
+					</body>
+				</html>
+		  	`;
+
 	});
 
 	context.subscriptions.push(disposable);
