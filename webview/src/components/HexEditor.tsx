f8cc1a45ea313a640e7f4aa18b43de8032a8f686
Oliver Sun <oliver@sunhx.cn>
2024-03-23 16:50:19 +0800

    hover and show bin in hex editor

--- webview/src/components/HexEditor.tsx
+++ webview/src/components/HexEditor.tsx
@@ -7,21 +7,26 @@ interface Props {
   sizeByteHighlight: number
 }
 
-class HexEditor extends Component<Props> {
+interface State {
+  popover_display: string,
+  popover_left: number,
+  popover_top: number,
+  popover_str: string
+}
+
+class HexEditor extends Component<Props, State> {
   constructor(props: Props) {
     super(props);
 
     this.state = {
-      offsetHighlight: 0,
-      sizeByteHighlight: 0
+      popover_display: "none",
+      popover_left: 0,
+      popover_top: 0,
+      popover_str: ""
     }
-  }
 
-  updateHighlightSetting(offset: number, size: number) {
-    this.setState({
-      offsetHighlight: offset,
-      sizeByteHighlight: size
-    })
+    this.onMouseEnter = this.onMouseEnter.bind(this);
+    this.onMouseLeave = this.onMouseLeave.bind(this);
   }
 
   render() {
@@ -55,16 +60,48 @@ class HexEditor extends Component<Props> {
                 hexArray.map((hex, i) => (
                   ( (16*index+i)>=this.props.offsetHighlight && 
                     (16*index+i) < this.props.offsetHighlight + this.props.sizeByteHighlight)?
-                    <span className='HexEditor-highlight'>{hex}</span>:
-                    <span>{hex}</span>
+                    <span className='HexEditor-highlight' onMouseEnter={this.onMouseEnter} onMouseLeave={this.onMouseLeave}>{hex}</span>:
+                    <span onMouseEnter={this.onMouseEnter} onMouseLeave={this.onMouseLeave}>{hex}</span>
                   ))
               }
             </div>
           ))
         }
         </div>
+        <div className='HedEditor-popover' style={{
+          display: this.state.popover_display, 
+          left: this.state.popover_left, 
+          top: this.state.popover_top
+          }}>
+          {this.state.popover_str}
+        </div>
       </div>
     );
+  }
+
+  onMouseEnter(event: React.MouseEvent<HTMLDivElement, MouseEvent>) {
+    const element = event.currentTarget;
+    const value = element.innerText;
+    const pos = element.getBoundingClientRect();
+    const left = pos.left;
+    const top = pos.top;
+    const bin_str = parseInt(value, 16).toString(2).padStart(8, '0').replace(/(.{4})/g, '$1 ')
+    console.log(`enter: ${value}`);
+    this.setState({
+      popover_display: 'block',
+      popover_left: left,
+      popover_top: top-45,
+      popover_str: bin_str
+    })
+  }
+
+  onMouseLeave(event: React.MouseEvent<HTMLDivElement, MouseEvent>) {
+    const element = event.currentTarget;
+    const value = element.innerText;
+    console.log(`leave: ${value}`);
+    this.setState({
+      popover_display: 'none'
+    })
   }
 }
 
